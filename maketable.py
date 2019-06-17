94b59aed47cef0e5ee8df500a987067bfb4c047b
stephan <stephansmit@hotmail.com>
2019-06-17 11:49:18 +0200

    made cleean

--- maketable.py
+++ maketable.py
@@ -3,29 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
- 
+#table input 
 Tmin=-20
 Tmax=60
 Pmax=700
 Pmin=20
 Np = 10
 Nt = 10
+
 #discretization
 P_vec = np.geomspace(Pmin, Pmax, Np)
 T_vec = np.linspace(Tmin, Tmax, Nt)
-Pv = []
-Tv = []
-for P in P_vec:
-    tmpT=[]
-    tmpP=[]
-    for T in T_vec:
-        tmpT.append(T)
-        tmpP.append(P)
-    Tv.append(tmpT)
-    Pv.append(tmpP)
-T=np.array(Tv)
-P=np.array(Pv)
-
+T = np.array([[t for t in T_vec ] for p in P_vec])
+P = np.array([[p for t in T_vec ] for p in P_vec])
 
 #calculation of values
 df = pd.DataFrame(data= [T.flatten(), P.flatten()]).transpose()
@@ -38,21 +28,17 @@ df.to_csv('table.csv',sep=',')
 #make the contour map
 hdf = df.groupby(['T','P']).mean()
 hdfreset = hdf.reset_index()
-hdfreset.columns = ['a', 'b', 'occurrence']
-hdfpivot=hdfreset.pivot('a', 'b')
+hdfreset.columns = ['T', 'P', 'R']
+hdfpivot=hdfreset.pivot('T', 'P')
 X=hdfpivot.columns.levels[1].values
 Y=hdfpivot.index.values
 Z=hdfpivot.values
 Xi,Yi = np.meshgrid(X, Y)
 CS = plt.contourf(Yi, Xi, Z, alpha=0.7, cmap=plt.cm.jet);
+cbar = plt.colorbar(CS)
 plt.title('Density as a function of P and T')
 plt.xlabel('Temperature [Celcius]')
 plt.ylabel('Pressure [Bar]')
-cbar = plt.colorbar(CS)
 cbar.ax.set_ylabel('Density [kg/m^3]')
 plt.savefig("table.png")
-
-
-
-#
 
